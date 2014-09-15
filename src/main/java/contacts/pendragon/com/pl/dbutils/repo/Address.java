847c167cdbb7a34760703e246fe874b7f331a7b6
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2014-09-15 18:53:26 +0200

    Address model class

---
+++ src/main/java/contacts/pendragon/com/pl/dbutils/repo/Address.java
@@ -0,0 +1,9 @@
+package contacts.pendragon.com.pl.dbutils.repo;
+
+import contacts.pendragon.com.pl.dbutils.DBModel;
+
+/**
+ * Created by daniel on 15.09.14.
+ */
+public class Address extends DBModel{
+}
