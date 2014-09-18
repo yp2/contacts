18087576fcfc5cf1d70ea494650d5b79d2b17823
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2014-09-18 21:29:22 +0200

    Person Address list

---
+++ db_populate_address.sql
@@ -0,0 +1,17 @@
+﻿/*
+INSERT INTO ADDRESS (PERSON_ID, STREET, HOUSE_NO, 
+FLAT_NO, CITY, POST_CODE, COUNTRY, DESCRIPTION) VALUES 
+(2, 'Diamentowa', '23', '12', 'Lublin', '20-573', 'Polska', 
+'opis to andrzej sroka');
+
+INSERT INTO ADDRESS (PERSON_ID, STREET, HOUSE_NO, 
+FLAT_NO, CITY, POST_CODE, COUNTRY, DESCRIPTION) VALUES 
+(2, 'Czechowska', '23', null, 'Lublin', '20-456', 'Polska', 
+'opis to andrzej sroka 2');
+
+*/
+
+INSERT INTO ADDRESS (PERSON_ID, STREET, HOUSE_NO, 
+FLAT_NO, CITY, POST_CODE, COUNTRY, DESCRIPTION) VALUES 
+(3, 'Abramowicka', '11', '23', 'Lublin', '20-345', 'Polska', 
+'opis to ogrodnictwo');
